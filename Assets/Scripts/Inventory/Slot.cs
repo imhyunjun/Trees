43ee9cc790553e8e15215ba394cbc23796f9d6d4
imhyunjun <limhyunjun04@naver.com>
2020-10-02 14:58:10 +0900

    Delete Obejct

--- Assets/Scripts/Inventory/Slot.cs
+++ Assets/Scripts/Inventory/Slot.cs
@@ -7,7 +7,7 @@ public class Slot : MonoBehaviour
 {
     public Sprite slotDefaultSprite;        //슬록 기본 이미지
     public Sprite hasItemSprite;            //슬롯이 가지고 있는 아이템 이미지
-    Color slotColor;                 //아이템 투명도 조절(클릭)
+    Color slotColor;                        //아이템 투명도 조절(클릭)
     public Color tempColor;                 //임시 칼라 이것으로 직접 조절
     public bool isSlotHasItem;              //슬롯에 아이템이 있는지
     public GameObject hasItem;              //가지고 있는 아이템 이름
--- Assets/Scripts/Objects/Object.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Object : MonoBehaviour
-{
-    public int objDialogueStart;
-    public int objDialogueEnd;
-}
