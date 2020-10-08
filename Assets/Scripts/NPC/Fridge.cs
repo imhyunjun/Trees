aa5e4e2b5897daaf8c4b7beca229bfbfb096c463
imhyunjun <limhyunjun04@naver.com>
2020-10-08 14:41:45 +0900

    Modify Fridge Error

--- Assets/Scripts/NPC/Fridge.cs
+++ Assets/Scripts/NPC/Fridge.cs
@@ -10,9 +10,11 @@ public class Fridge : NPC
 
     public override void Interact()
     {
+        Debug.Log("2");
         //쩅그랑 소리 추가 이곳 아니면 알코올의 GetItem함수에 추가
         if (PlayerScan.instance.progressStatus == ProgressStatus.E_GetCashNCard)         //카드를 가진상태면
         {
+            Debug.Log("1");
             alcoholBottle.SetActive(true);
             Inventory.instance.GetItemInSlot(alcoholBottle);                             //술을 가져오고
             PlayerScan.instance.progressStatus = ProgressStatus.E_GetAlcholBottle;      //술을 가진 상태로 변경
