e34d967ad13d4ef7243c3c2b28266075cb8362f7
Bora-Jeong <50454596+Bora-Jeong@users.noreply.github.com>
2020-11-08 17:46:48 +0900

    playerScan

--- Assets/Scripts/Player/PlayerScan.cs
+++ Assets/Scripts/Player/PlayerScan.cs
@@ -21,10 +21,27 @@ public class PlayerScan : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 3.5f, LayerMask.GetMask("object"));
-            if (hit)
+            //RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 3.5f, LayerMask.GetMask("object"));
+            //if (hit)
+            //{
+            //    NPC npc = hit.transform.GetComponent<NPC>();
+            //    if (npc != null) npc.Interact();
+            //}
+
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 4f, LayerMask.GetMask("object"));
+            float minDist = float.PositiveInfinity;
+            int index = -1;
+            for(int i = 0; i < colliders.Length; i++)
             {
-                NPC npc = hit.transform.GetComponent<NPC>();
+                if(Vector3.Distance(transform.position, colliders[i].transform.position) < minDist) // 가장 거리가 가까운 npc 검색
+                {
+                    minDist = Vector3.Distance(transform.position, colliders[i].transform.position);
+                    index = i;
+                }
+            }
+            if (index != -1)
+            {
+                NPC npc = colliders[index].transform.GetComponent<NPC>();
                 if (npc != null) npc.Interact();
             }
         }
