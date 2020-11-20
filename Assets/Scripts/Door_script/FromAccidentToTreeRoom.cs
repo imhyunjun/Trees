e16ab3f04561996513ea38126eabf1f67d1042e0
imhyunjun <limhyunjun04@naver.com>
2020-11-20 22:47:53 +0900

    TreeRoom

---
+++ Assets/Scripts/Door_script/FromAccidentToTreeRoom.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FromAccidentToTreeRoom : Door
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ProgressStatus status = PlayerScan.instance.progressStatus;
+        if (status == ProgressStatus.E_JungWannaKillFather)
+            Inventory.instance.ChangeInteractObjectInInven(typeof(BrokenBottle), "Tree");
+    }
+}
